b841cf7b0c320557a1f5a467b6c62576c21cfa8c
Bruno0Vinicius <brunoviniciuse0@gmail.com>
2022-07-12 04:05:28 -0300

    att

---
+++ scripts/textscript.js
@@ -0,0 +1,5 @@
+const botaoEu = document.querySelector('#eu');
+
+botaoEu.addEventListener('click', () => {
+    document.getElementById("texto").innerHTML="Oi, eu sou o Bruno tenho 16 anos , moro em itapevi-SP.<br> Estudo desenvolvimento web a alguns meses <br> procuro por uma oportunidade para adquirir conhecimento e experiência na área de front e back-end ";
+});
